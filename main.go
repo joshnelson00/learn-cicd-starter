7a5cc59f1156e3a6b910841bcfd644351f262f9c
Josh Nelson <joshuaknelson1202@icloud.com>
2025-07-19 11:09:06 -0500

    edited main.go with func and added linting for ci.yml

--- main.go
+++ main.go
@@ -25,12 +25,6 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
-
-func unused() {
-    // this function does nothing
-    // and is called nowhere
-}
-
 func main() {
 
 	
